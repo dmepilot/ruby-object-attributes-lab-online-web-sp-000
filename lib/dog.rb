5f4e3b6ca297d6f2d9327c7823bb5c3304007a80
Dean Erickson <dmepilot@yahoo.com>
2020-06-16 17:57:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def initialize(dogs_name)
-    @this_dogs_name = dogs_name
-  end
+  #def initialize(dogs_name)
+   # @name = dogs_name
+  #end
 
   def name
-    @this_dogs_name
+    @name
   end
   
   def name=(new_dogs_name)
-    @this_dogs_name = new_dogs_name
+    @name = new_dogs_name
   
   def breed=(dogs_breed)
     @this_dogs_breed = dogs_breed
